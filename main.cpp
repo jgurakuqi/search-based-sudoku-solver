e80eaa6ddfea70d26c0363295c665d9b1980c1b9
Jurgen Gurakuqi <856180@stud.unive.it>
2022-02-28 19:52:19 +0100

    Added matrix creation (Not Working)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
 
+
+using namespace std;
 
 
 int main()
 {
-	std::cout << "Hello !";
+	vector<vector<char>> sudokuField(9);
+	const int FIELD_SIZE = 9;
+	for (int i = 0; i < FIELD_SIZE; i++)
+	{
+		for (int j = 0; j < FIELD_SIZE; i++)
+		{
+			sudokuField[i].push_back(".");
+		}
+	}
+	for (int i = 0; i < FIELD_SIZE; i++)
+	{
+		for (int j = 0; j < FIELD_SIZE; i++)
+		{
+			cout << sudokuField[i][j] << " ";
+		}
+		cout << "\n"; 
+	}
 	return 0;
 }
