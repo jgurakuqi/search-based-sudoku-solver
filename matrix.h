515791c0b6da9c2ae33ac6645368947914c252de
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-01 20:04:50 +0100

    Creation of basic examples of input for the CP

---
+++ matrix.h
@@ -0,0 +1,36 @@
+
+#include <vector>
+#define FIELD_SIZE 9
+
+
+// template<class T>
+// class matrix
+// {
+// private:
+
+//     std::vector<std::vector<T>> sudokuField;
+    
+// public:
+
+    
+
+//     matrix()
+//     {
+//         sudokuField(FIELD_SIZE, vector<T>(FIELD_SIZE, '.'));
+//     }
+
+//     ~matrix();
+
+//     T& operator[](int index)
+//     {
+
+//     }
+// };
+
+// matrix::matrix()
+// {
+// }
+
+// matrix::~matrix()
+// {
+// }
