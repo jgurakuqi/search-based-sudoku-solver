aaed80b01c4b6c5e4f9882a1ddd04b68ffbed1c3
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-01 11:42:25 +0100

    Handling the sudoku field creation
    Co-authored-by: Elsa  <elaaj@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
-
+#define FIELD_SIZE 9
 
 using namespace std;
+
+void createEmptyMatrix()
+{
+
+}
+
+template<class T>
+ostream& operator<<(ostream& stream, const std::vector<T>& values)
+{
+    copy( begin(values), end(values), ostream_iterator<T>(stream, " ") );
+    stream << '\n';
+    return stream;
+}
 
 
 int main()
 {
-	vector<vector<char>> sudokuField(9);
-	const int FIELD_SIZE = 9;
+	cout << " CHECK -1 ";
+	vector<vector<char>> sudokuField;
+	cout << " CHECK 0 ";
 	for (int i = 0; i < FIELD_SIZE; i++)
 	{
-		for (int j = 0; j < FIELD_SIZE; i++)
+		vector<char> row;
+		cout << " CHECK 0 - 1 ";
+		for (int j = 0; j < FIELD_SIZE; j++)
 		{
-			sudokuField[i].push_back(".");
+			row.push_back('.');
 		}
+		sudokuField.push_back(row);
 	}
-	for (int i = 0; i < FIELD_SIZE; i++)
-	{
-		for (int j = 0; j < FIELD_SIZE; i++)
-		{
-			cout << sudokuField[i][j] << " ";
-		}
-		cout << "\n"; 
-	}
+	cout << " CHECK 1 ";
+	cout << sudokuField;
+	// for (int i = 0; i < FIELD_SIZE; i++)
+	// {
+	// 	for (int j = 0; j < FIELD_SIZE; j++)
+	// 	{
+	// 		cout << sudokuField[i][j] << " ";
+	// 	}
+	// 	cout << "\n"; 
+	// }
 	return 0;
 }
