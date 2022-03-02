62eef0cc4091150ef4895973fd7fec90f8335fb8
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-02 23:06:28 +0100

    Bug fix on solveSudokuI function

--- constraint_propagation_based_solver.cpp
+++ constraint_propagation_based_solver.cpp
@@ -6,16 +6,16 @@ vector<vector<int>> solveSudoku(vector<vector<int>> sudokuField)
     {
         return nullptr;
     }
-    vector<vector<vector<int>>> sudokuCellsDomains = createSudokuDomain(sudokuField);
-    return solveSudokuI(sudokuField, sudokuCellsDomains);
+    return solveSudokuI(sudokuField, createSudokuDomain(sudokuField));
 }
 
 vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<vector<int>>> sudokuCellsDomains)
 {
+    // Constraint propagation step
     vector<vector<vector<int>>> sudokuCellsDomains = updateSudokuDomain(sudokuCellsDomains);
     int emptyCellRow, emptyCellColumn = -1;
     vector<vector<int>> result = nullptr;
-    // Constraint propagation step
+
     for (int i = 0; i < FIELD_SIZE; i++)
     {
         for (int j = 0; j < count; j++)
@@ -32,15 +32,19 @@ vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<
         }
     }
     // Potresti fare differentemente il controllo, per vedere prima se la matrice e' gia completa.
-    if (emptyCellRow != -1)
+    if (emptyCellColumn == -1)
     {
         return sudokuField;
     }
-    while (!sudokuCellsDomains[i][j].empty() && result != nullptr)
+    while (!sudokuCellsDomains[emptyCellRow][emptyCellColumn].empty() && result != nullptr)
     {
         vector<vector<int>> nextStepField = sudokuField;
         nextStepField[i][j] = sudokuCellsDomains[i][j][0];
         result = solveSudokuI(nextStepField, sudokuCellsDomains);
+        if (result == nullptr)
+        {
+            sudokuCellsDomains[emptyCellRow][emptyCellColumn].remove(0);
+        }
     }
     return result;
 }
