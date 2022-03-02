00f57ed8e21abb38b6eb82f70381bf2fa138c04e
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-02 22:44:18 +0100

    Defined basics for the CP based solver

---
+++ constraint_propagation_based_solver.cpp
@@ -0,0 +1,46 @@
+#define FIELD_SIZE 9
+
+vector<vector<int>> solveSudoku(vector<vector<int>> sudokuField)
+{
+    if (sudokuField == nullptr)
+    {
+        return nullptr;
+    }
+    vector<vector<vector<int>>> sudokuCellsDomains = createSudokuDomain(sudokuField);
+    return solveSudokuI(sudokuField, sudokuCellsDomains);
+}
+
+vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<vector<int>>> sudokuCellsDomains)
+{
+    vector<vector<vector<int>>> sudokuCellsDomains = updateSudokuDomain(sudokuCellsDomains);
+    int emptyCellRow, emptyCellColumn = -1;
+    vector<vector<int>> result = nullptr;
+    // Constraint propagation step
+    for (int i = 0; i < FIELD_SIZE; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            if (sudokuField[i][j] == 0 && sudokuCellsDomains[i][j].empty())
+            {
+                return nullptr;
+            }
+            if (emptyCellColumn == -1 && sudokuField[i][j] == 0)
+            {
+                emptyCellRow = i;
+                emptyCellColumn = j;
+            }
+        }
+    }
+    // Potresti fare differentemente il controllo, per vedere prima se la matrice e' gia completa.
+    if (emptyCellRow != -1)
+    {
+        return sudokuField;
+    }
+    while (!sudokuCellsDomains[i][j].empty() && result != nullptr)
+    {
+        vector<vector<int>> nextStepField = sudokuField;
+        nextStepField[i][j] = sudokuCellsDomains[i][j][0];
+        result = solveSudokuI(nextStepField, sudokuCellsDomains);
+    }
+    return result;
+}
