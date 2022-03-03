bb2e8455ce3ea4837a3f3525387a4dff8458e1e3
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-04 00:01:31 +0100

    First working solution of constraint propagation

--- constraint_propagation_based_solver.cpp
+++ constraint_propagation_based_solver.cpp
@@ -1,28 +1,82 @@
+#include <algorithm>
+#include <vector>
+#include <iterator>
 #define FIELD_SIZE 9
 
-vector<vector<int>> solveSudoku(vector<vector<int>> sudokuField)
-{
-    if (sudokuField == nullptr)
-    {
-        return nullptr;
-    }
-    return solveSudokuI(sudokuField, createSudokuDomain(sudokuField));
-}
+using namespace std;
 
-vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<vector<int>>> sudokuCellsDomains)
+/**
+ * @brief The following function performs the constraint propagation over the column, row and block related
+ * to the cell [row][col], removing it's value from any domain
+ *
+ * @param sudokuField
+ * @param domainsMatrix
+ * @param row
+ * @param col
+ */
+void updateDomainsX(vector<vector<int>> &sudokuField, vector<vector<vector<int>>> &domainsMatrix, int row, int col)
+{
+    if (row == -1)
+        return;
+    int blockRowStart, blockColStart;
+    // Domain deallocation
+    domainsMatrix[row][col].resize(0);
+    // Row and column control
+    for (int i = 0; i < FIELD_SIZE; i++)
+    {
+        if (sudokuField[row][i] == 0 && i != col)
+        {
+            domainsMatrix[row][i].erase(
+                remove(
+                    domainsMatrix[row][i].begin(),
+                    domainsMatrix[row][i].end(),
+                    sudokuField[row][col]),
+                domainsMatrix[row][i].end());
+        }
+        if (sudokuField[i][col] == 0 && i != row)
+        {
+            domainsMatrix[i][col].erase(
+                remove(
+                    domainsMatrix[i][col].begin(),
+                    domainsMatrix[i][col].end(),
+                    sudokuField[row][col]),
+                domainsMatrix[i][col].end());
+        }
+    }
+    // Block control
+    blockRowStart = row - (row % 3);
+    blockColStart = col - (col % 3);
+    for (int i = blockRowStart; i < (blockRowStart + 2); i++)
+    {
+        for (int j = blockColStart; j < (blockColStart + 2); j++)
+        {
+            if (sudokuField[i][j] == 0 && i != row && j != col)
+            {
+                domainsMatrix[i][j].erase(
+                    remove(
+                        domainsMatrix[i][j].begin(),
+                        domainsMatrix[i][j].end(),
+                        sudokuField[row][col]),
+                    domainsMatrix[i][j].end());
+            }
+        }
+    }
+};
+
+vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<vector<int>>> domainsMatrix, int actRow, int actCol)
 {
     // Constraint propagation step
-    vector<vector<vector<int>>> sudokuCellsDomains = updateSudokuDomain(sudokuCellsDomains);
+    updateDomainsX(sudokuField, domainsMatrix, actRow, actCol);
     int emptyCellRow, emptyCellColumn = -1;
-    vector<vector<int>> result = nullptr;
+    vector<vector<int>> result;
 
     for (int i = 0; i < FIELD_SIZE; i++)
     {
-        for (int j = 0; j < count; j++)
+        for (int j = 0; j < FIELD_SIZE; j++)
         {
-            if (sudokuField[i][j] == 0 && sudokuCellsDomains[i][j].empty())
+            if (sudokuField[i][j] == 0 && domainsMatrix[i][j].empty())
             {
-                return nullptr;
+                return {};
             }
             if (emptyCellColumn == -1 && sudokuField[i][j] == 0)
             {
@@ -36,15 +90,25 @@ vector<vector<int>> solveSudokuI(vector<vector<int>> sudokuField, vector<vector<
     {
         return sudokuField;
     }
-    while (!sudokuCellsDomains[emptyCellRow][emptyCellColumn].empty() && result != nullptr)
+    while (!domainsMatrix[emptyCellRow][emptyCellColumn].empty() && result.empty())
     {
         vector<vector<int>> nextStepField = sudokuField;
-        nextStepField[i][j] = sudokuCellsDomains[i][j][0];
-        result = solveSudokuI(nextStepField, sudokuCellsDomains);
-        if (result == nullptr)
+        nextStepField[emptyCellRow][emptyCellColumn] = domainsMatrix[emptyCellRow][emptyCellColumn][0];
+        result = solveSudokuI(nextStepField, domainsMatrix, emptyCellRow, emptyCellColumn);
+        if (result.empty())
         {
-            sudokuCellsDomains[emptyCellRow][emptyCellColumn].remove(0);
+            domainsMatrix[emptyCellRow][emptyCellColumn].erase(domainsMatrix[emptyCellRow][emptyCellColumn].begin());
         }
     }
     return result;
+}
+
+vector<vector<int>> solveSudoku(vector<vector<int>> sudokuField, vector<vector<vector<int>>> domainsMatrix)
+{
+    //@TODO: METTERE QUI LA CREAZIONE DELLA DOMAIN MATRIX.
+    // if (sudokuField == nullptr)
+    // {
+    //     return nullptr;
+    // }
+    return solveSudokuI(sudokuField, domainsMatrix, -1, -1);
 }
--- main.cpp
+++ main.cpp
@@ -1,68 +1,148 @@
 #include <iostream>
-#include <iterator>
-#include "matrix.h"
-
+#include "constraint_propagation_based_solver.cpp"
 using namespace std;
-
 
 /**
  * @brief This method prints the content of the vector values.
- * 
- * @tparam T 
- * @param stream 
- * @param values 
- * @return ostream& 
+ *
+ * @tparam T
+ * @param stream
+ * @param values
+ * @return ostream&
  */
-template<class T>
-ostream& operator<<(ostream& stream, const std::vector<T>& values)
+template <class T>
+ostream &operator<<(ostream &stream, const std::vector<T> &values)
 {
 	for (auto &value : values)
 	{
 		stream << value << " ";
 	}
-    stream << '\n';
-    return stream;
+	// stream << '\n';
+	return stream;
 }
 
+/**
+ * @brief The following function performs the constraint propagation over the column, row and block related
+ * to the cell [row][col], removing it's value from any domain
+ *
+ * @param sudokuField
+ * @param domainsMatrix
+ * @param row
+ * @param col
+ */
+void updateDomains(vector<vector<int>> &sudokuField, vector<vector<vector<int>>> &domainsMatrix, int row, int col)
+{
+	int blockRowStart, blockColStart;
+	// Domain deallocation
+	domainsMatrix[row][col].resize(0);
+	// Row and column control
+	for (int i = 0; i < FIELD_SIZE; i++)
+	{
+		if (sudokuField[row][i] == 0 && i != col)
+		{
+			domainsMatrix[row][i].erase(
+				remove(
+					domainsMatrix[row][i].begin(),
+					domainsMatrix[row][i].end(),
+					sudokuField[row][col]),
+				domainsMatrix[row][i].end());
+		}
+		if (sudokuField[i][col] == 0 && i != row)
+		{
+			domainsMatrix[i][col].erase(
+				remove(
+					domainsMatrix[i][col].begin(),
+					domainsMatrix[i][col].end(),
+					sudokuField[row][col]),
+				domainsMatrix[i][col].end());
+		}
+	}
+	// Block control
+	blockRowStart = row - (row % 3);
+	blockColStart = col - (col % 3);
+	for (int i = blockRowStart; i < (blockRowStart + 2); i++)
+	{
+		for (int j = blockColStart; j < (blockColStart + 2); j++)
+		{
+			if (sudokuField[i][j] == 0 && i != row && j != col)
+			{
+				domainsMatrix[i][j].erase(
+					remove(
+						domainsMatrix[i][j].begin(),
+						domainsMatrix[i][j].end(),
+						sudokuField[row][col]),
+					domainsMatrix[i][j].end());
+			}
+		}
+	}
+};
 
+void initializeDomainsMatrix(vector<vector<int>> &sudokuField, vector<vector<vector<int>>> &domainsMatrix)
+{
+	for (int i = 0; i < FIELD_SIZE; i++)
+	{
+		for (int j = 0; j < FIELD_SIZE; j++)
+		{
+			if (sudokuField[i][j] != 0)
+			{
+				updateDomains(sudokuField, domainsMatrix, i, j);
+			}
+		}
+	}
+};
 
 int main()
 {
-	vector<vector<int>> sudokuField
-	{
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0},
-		// {0,0,0,0,0,0,0,0,0}
-		{3,7,0,5,0,0,0,0,6},
-		{0,0,0,3,6,0,0,1,2},
-		{0,0,0,0,9,1,7,5,0},
-		{0,0,0,1,5,4,0,7,0},
-		{0,0,3,0,7,0,6,0,0},
-		{0,5,0,6,3,8,0,0,0},
-		{0,6,4,9,8,0,0,0,0},
-		{5,9,0,0,2,6,0,0,0},
-		{2,0,0,0,0,5,0,6,4}
+	vector<vector<int>> sudokuField{
+		{3, 7, 0, 5, 0, 0, 0, 0, 6},
+		{0, 0, 0, 3, 6, 0, 0, 1, 2},
+		{0, 0, 0, 0, 9, 1, 7, 5, 0},
+		{0, 0, 0, 1, 5, 4, 0, 7, 0},
+		{0, 0, 3, 0, 7, 0, 6, 0, 0},
+		{0, 5, 0, 6, 3, 8, 0, 0, 0},
+		{0, 6, 4, 9, 8, 0, 0, 0, 0},
+		{5, 9, 0, 0, 2, 6, 0, 0, 0},
+		{2, 0, 0, 0, 0, 5, 0, 6, 4}};
 
-	};
-	vector<vector<vector<int>>> sudokuCellsDomains
+	vector<vector<int>> sudokuField2{
+		{6, 7, 0, 5, 6, 0, 0, 0, 6},
+		{0, 0, 0, 3, 6, 0, 0, 1, 2},
+		{0, 0, 0, 0, 9, 1, 7, 5, 0},
+		{0, 0, 0, 1, 5, 4, 0, 7, 0},
+		{0, 0, 3, 0, 7, 0, 6, 0, 0},
+		{0, 5, 0, 6, 3, 8, 0, 0, 0},
+		{0, 6, 4, 9, 8, 0, 0, 0, 0},
+		{5, 9, 0, 0, 2, 6, 0, 0, 0},
+		{2, 0, 0, 0, 0, 5, 0, 6, 4}};
+
+	vector<vector<vector<int>>> domainsMatrix(FIELD_SIZE, vector<vector<int>>(FIELD_SIZE, vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+	vector<vector<vector<int>>> domainsMatrix2(FIELD_SIZE, vector<vector<int>>(FIELD_SIZE, vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+
+	initializeDomainsMatrix(sudokuField, domainsMatrix);
+	initializeDomainsMatrix(sudokuField2, domainsMatrix2);
+
+	// cout << sudokuField;
+	// cout << domainsMatrix;
+
+	for (int i = 0; i < FIELD_SIZE; i++)
 	{
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}},
-		{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}}
-	};
-	cout << sudokuField;
-	cout << sudokuCellsDomains;
+		cout << "{ ";
+		for (int j = 0; j < FIELD_SIZE; j++)
+		{
+			cout << domainsMatrix[i][j] << ", ";
+		}
+		cout << " }\n";
+	}
+	for (int i = 0; i < FIELD_SIZE; i++)
+	{
+		cout << "{ ";
+		for (int j = 0; j < FIELD_SIZE; j++)
+		{
+			cout << domainsMatrix2[i][j] << ", ";
+		}
+		cout << " }\n";
+	}
+
+	cout << solveSudoku(sudokuField, domainsMatrix);
 	return 0;
 }
