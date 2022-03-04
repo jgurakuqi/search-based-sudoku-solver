af6ecf88bb3e271e1a65e03d51a1bd282165b2e9
Jurgen Gurakuqi <856180@stud.unive.it>
2022-03-04 18:06:17 +0100

    Removed comments from main

--- main.cpp
+++ main.cpp
@@ -3,43 +3,6 @@
 #include "relaxation_labeling_based_solver.h"
 using namespace std;
 
-// /**
-//  * @brief This method prints the content of the vector values.
-//  *
-//  * @tparam T
-//  * @param stream
-//  * @param values
-//  * @return ostream&
-//  */
-// template <class T>
-// ostream &operator<<(ostream &stream, const std::vector<T> &values)
-// {
-// 	for (auto &value : values)
-// 	{
-// 		stream << value << " ";
-// 	}
-// 	// stream << '\n';
-// 	return stream;
-// }
-
-// void printSudokuField(vector<vector<int>> sudokuField)
-// {
-// 	if (sudokuField.empty())
-// 	{
-// 		return;
-// 	}
-
-// 	for (int i = 0; i < FIELD_SIZE; i++)
-// 	{
-// 		cout << "| ";
-// 		for (int j = 0; j < FIELD_SIZE; j++)
-// 		{
-// 			cout << sudokuField[i][j] << " ";
-// 		}
-// 		cout << "|\n";
-// 	}
-// }
-
 int main()
 {
 	// FUNZIONANTE
@@ -86,34 +49,6 @@ int main()
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}};
-
-	// vector<vector<vector<int>>> domainsMatrix(FIELD_SIZE, vector<vector<int>>(FIELD_SIZE, vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}));
-	// vector<vector<vector<int>>> domainsMatrix2(FIELD_SIZE, vector<vector<int>>(FIELD_SIZE, vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}));
-
-	// initializeDomainsMatrix(sudokuField, domainsMatrix);
-	// initializeDomainsMatrix(sudokuField2, domainsMatrix2);
-
-	// cout << sudokuField;
-	// cout << domainsMatrix;
-
-	// for (int i = 0; i < FIELD_SIZE; i++)
-	// {
-	// 	cout << "{ ";
-	// 	for (int j = 0; j < FIELD_SIZE; j++)
-	// 	{
-	// 		cout << domainsMatrix[i][j] << ", ";
-	// 	}
-	// 	cout << " }\n";
-	// }
-	// for (int i = 0; i < FIELD_SIZE; i++)
-	// {
-	// 	cout << "{ ";
-	// 	for (int j = 0; j < FIELD_SIZE; j++)
-	// 	{
-	// 		cout << domainsMatrix2[i][j] << ", ";
-	// 	}
-	// 	cout << " }\n";
-	// }
 
 	printSudokuField(solveSudoku(sudokuField));
 	cout << "\n";
